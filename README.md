2218e9e1a6a8863af70b173e46ddc9c892222697
Amir Eghtedar <amir99.en@gmail.com>
2021-07-30 11:40:11 +0430

    add setup and install scripts

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ including:
   ZSH : rc + aliases + env + path
   Neovim : init conf + vim-plug + plugin conf
   bash : rc
+
+Installation:
+  1 - get the files: 
+    git clone https://github.com/amir-99/dotfiles ~/.local/dotfiles
+  2- change shell permissions:
+    chmod u+x ~/.local/dotfiles/*.sh
+  3- install requirements
+    ~/.local/dotfiles/install.sh
+  4- setup the dotfiles
+    ~/.local/dotfiles/setup.sh
+  5- change shell run:
+    chsh $USER
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+## setup zsh
+
+dotdir="$PWD"
+
+[ -d "${HOME}/.local" ] || { mkdir "${HOME}/.local" && chmod u+rw -R "${HOME}/.local"; }
+# get pl10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${HOME}/.local/powerlevel10k
+ln -s "${dotdir}/zshd/.zshrc" "${HOME}/.zshrc"
+[ -e "${HOME}/.cache/zshhistory" ] || { touch "${HOME}/.cache/zshhistory" && chmod u+rw "${HOME}/.cache/zshhistory"; }
+
+# setup neovim
+nvimdir="${HOMW}/.config/nvim"
+[ -d "${nvimdir}" ] || { mkdir -p "${nvimdir}/autoload" && chmod u+rw -R "${nvimdir}"; }
+[ -d "${nvimdir}/vimplug/plug-conf"] || mkdir -p "${nvimdir}/vimplug/plug-conf"
+curl -flo "${nvimdir}/autoload/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s "${dotdir}/init.vim" "${nvimdir}/init.vim"
+ln -s "${dotdir}/conf.vim" "${nvimdir}/conf.vim"
+ln -s "${dotdir}/keybindings.vim" "${nvimdir}/keybindings.vim"
+ln -s "${dotdir}/coc-settings.json" "${nvimdir}/coc-settings.json"
+ln -s "${dotdir}/plug.vim" "${nvimdir}/vimplug/plug.vim"
+ln -s "${dotdir}/floaterm.vim" "${nvimdir}/vimplug/plug-conf/floaterm.vim"
+ln -s "${dotdir}/coc.vim" "${nvimdir}/vimplug/plug-conf/floaterm.vim"
+ln -s "${dotdir}/NERDTree.vim" "${nvimdir}/vimplug/pulg-conf/NERTree.vim"
+

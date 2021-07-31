7b3fd526e664f071eb5262038da9bde2e25103d6
Amir Eghtedar <amir99.en@gmail.com>
2021-07-31 16:27:20 +0430

    fixing small bugs

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # install minimum requirements
 
-[ -e "/etc/apt/sources.list.d/apt-fast.list" ] || { echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main\ndeb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main" > /etc/apt/sources.list.d/apt-fast.list && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B }
+[ -e "/etc/apt/sources.list.d/apt-fast.list" ] || { echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main\ndeb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main" > /etc/apt/sources.list.d/apt-fast.list && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B; }
 
 package_list="git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlighting zsh-autosuggestions apt-fast"
-apt update || { echo "failed to update the package list"; exit 1;}
+apt update || { echo "failed to update the package list"; exit 1; }
 
 echo "installing packages ..."
 
--- setup.sh
+++ setup.sh
@@ -10,18 +10,23 @@ dotdir="$PWD"
 ln -s -f "${dotdir}/zshd/.zshrc" "${HOME}/.zshrc"
 [ -e "${HOME}/.cache/zshhistory" ] || { touch "${HOME}/.cache/zshhistory" && chmod u+rw "${HOME}/.cache/zshhistory"; }
 
+echo "zsh setup complete"
+
 # setup neovim
 nvimdir="${HOME}/.config/nvim"
 [ -d "${nvimdir}" ] || { mkdir -p "${nvimdir}/autoload" && chmod u+rw -R "${nvimdir}"; }
-[ -d "${nvimdir}/vimplug/plug-conf"] || mkdir -p "${nvimdir}/vimplug/plug-conf"
+[ -d "${nvimdir}/vimplug/plug-conf" ] || mkdir -p "${nvimdir}/vimplug/plug-conf"
 [ -e "${nvimdir}/autoload/plug.vim" ] || curl -flo "${nvimdir}/autoload/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s -f "${dotdir}/nvimd/init.vim" "${nvimdir}/init.vim"
 ln -s -f "${dotdir}/nvimd/conf.vim" "${nvimdir}/conf.vim"
 ln -s -f "${dotdir}/nvimd/keybindings.vim" "${nvimdir}/keybindings.vim"
 ln -s -f "${dotdir}/nvimd/coc-settings.json" "${nvimdir}/coc-settings.json"
-ln -s -f"${dotdir}/nvimd/plug.vim" "${nvimdir}/vimplug/plug.vim"
+ln -s -f "${dotdir}/nvimd/plug.vim" "${nvimdir}/vimplug/plug.vim"
 ln -s -f "${dotdir}/nvimd/floaterm.vim" "${nvimdir}/vimplug/plug-conf/floaterm.vim"
 ln -s -f "${dotdir}/nvimd/coc.vim" "${nvimdir}/vimplug/plug-conf/floaterm.vim"
 ln -s -f "${dotdir}/nvimd/NERDTree.vim" "${nvimdir}/vimplug/plug-conf/NERDTree.vim"
 
+echo "nvim setup complete"
+
+exit 0 

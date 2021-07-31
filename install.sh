003442812c33d326a33e379a192297b508f822e3
Amir Eghtedar <amir99.en@gmail.com>
2021-07-31 13:49:39 +0430

    resolve privilige issues in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/usr/bin/sudo sh
 
 # get sudo priviliges
-echo "$(whoami)"
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+#echo "$(whoami)"
+#[ "$UID" -eq 0 ] || exec sudo "${0}" "${@}" 
 
 # install minimum requirements
-package_list="neovim git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlighting zsh-autosuggestions"
+package_list="neovim git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlighting zsh-autosuggestions apt-fast"
 apt update || { echo "failed to update the package list"; exit 1;}
 echo "installing packages ..."
-for pakage in $package_list;do
+for package in $package_list;do
   echo "install ${package}"
   apt install $package > /dev/null && echo "installed ${package}" || echo "failed to install ${package}"
 done

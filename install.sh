75aaa72e1a2eab5b3ea01b3378b3cdc36c09d39e
Amir Eghtedar <amir99.en@gmail.com>
2021-07-31 14:03:13 +0430

    change nvim installation from default pass to nvim 5 app image

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/sudo sh
 
-# get sudo priviliges
-#echo "$(whoami)"
-#[ "$UID" -eq 0 ] || exec sudo "${0}" "${@}" 
-
 # install minimum requirements
-package_list="neovim git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlighting zsh-autosuggestions apt-fast"
+package_list="git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlighting zsh-autosuggestions apt-fast"
 apt update || { echo "failed to update the package list"; exit 1;}
+
 echo "installing packages ..."
+
 for package in $package_list;do
-  echo "install ${package}"
+  echo "installing ${package} ..."
   apt install $package > /dev/null && echo "installed ${package}" || echo "failed to install ${package}"
 done
 
+echo "install nvim 5"
+curl -flo ~/.local/sources/nvim https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage && cp ~/.local/sources/nvim/nvim.appimage /usr/bin/nvim && echo "installe nvim" || echo "failed to install nvim"
+
+echo "done"
+exit 0

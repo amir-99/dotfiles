9b118ec8ade1387911265455fedf2f08b42237dd
Amir Eghtedar <amir99.en@gmail.com>
2021-08-01 13:54:47 +0430

    better install.sh reports

--- install.sh
+++ install.sh
@@ -8,14 +8,16 @@ package_list="git curl wget tree htop python3 python3-pip zsh zsh-syntax-highlig
 apt update || { echo "failed to update the package list"; exit 1; }
 
 echo "installing packages ..."
-
+failed_packeges=""
 for package in $package_list;do
   echo "installing ${package} ..."
-  apt install $package -y && echo "installed ${package}" || echo "failed to install ${package}"
+  apt install $package -y && echo "installed ${package}" || { echo "failed to install ${package}"; failed_packeges="${failed_packeges} ${package}"; }
 done
 
 echo "install nvim 5"
-[ -e /usr/bin/nvim ] || { mkdir -p /opt/sources/nvim &&  wget -O /opt/sources/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage && chmod a+rx /opt/sources/nvim/nvim.appimag && cp /opt/sources/nvim/nvim.appimage /usr/bin/nvim && chmod a+rx /usr/bin/nvim && echo "installed nvim" || echo "failed to install nvim"; }
+[ -e /usr/bin/nvim ] || { mkdir -p /opt/sources/nvim &&  wget -O /opt/sources/nvim/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage && chmod a+rx /opt/sources/nvim/nvim.appimag && cp /opt/sources/nvim/nvim.appimage /usr/bin/nvim && chmod a+rx /usr/bin/nvim && echo "installed nvim" || echo "failed to install nvim"
 
+echo "Failed to install ${failed_packeges}\nTry installing the manually or rerun the script"
+echo "install nodejs for coc manually."
 echo "done"
 exit 0

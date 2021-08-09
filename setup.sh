0bb2700500665ae9b60bfad8fd7ffb767fc8bab2
Amir Eghtedar <amir99.en@gmail.com>
2021-08-09 22:57:32 +0430

    shorten lines for command groups

--- setup.sh
+++ setup.sh
@@ -4,17 +4,23 @@
 
 dotdir="/home/${USER}/.local/dotfiles"
 
-[ -d "${HOME}/.local" ] || { mkdir "${HOME}/.local" && chmod u+rw -R "${HOME}/.local"; }
+[ -d "${HOME}/.local" ] || {
+                            mkdir "${HOME}/.local" &&
+                            chmod u+rw -R "${HOME}/.local"; }
 # get pl10k
 [ -d "${HOME}/.local/powerlevel10k" ] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${HOME}/.local/powerlevel10k
 ln -s -f "${dotdir}/zshd/.zshrc" "${HOME}/.zshrc"
-[ -e "${HOME}/.cache/zshhistory" ] || { touch "${HOME}/.cache/zshhistory" && chmod u+rw "${HOME}/.cache/zshhistory"; }
+[ -e "${HOME}/.cache/zshhistory" ] || {
+                                      touch "${HOME}/.cache/zshhistory" &&
+                                      chmod u+rw "${HOME}/.cache/zshhistory"; }
 
 echo "zsh setup complete"
 
 # setup neovim
 nvimdir="${HOME}/.config/nvim"
-[ -d "${nvimdir}" ] || { mkdir -p "${nvimdir}/autoload" && chmod u+rw -R "${nvimdir}"; }
+[ -d "${nvimdir}" ] || {
+                        mkdir -p "${nvimdir}/autoload" &&
+                        chmod u+rw -R "${nvimdir}"; }
 [ -d "${nvimdir}/vimplug/plug-conf" ] || mkdir -p "${nvimdir}/vimplug/plug-conf"
 [ -d "${nvimdir}/colors" ] || mkdir -p "${nvimdir}/colors"
 [ -e "${nvimdir}/autoload/plug.vim" ] || curl -flo "${nvimdir}/autoload/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

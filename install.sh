d60359769a6e83c8f96792d66cdd1827c19c0dee
Amir Eghtedar <amir99.en@gmail.com>
2021-08-10 12:54:44 +0430

    clang tools install config

--- install.sh
+++ install.sh
@@ -2,14 +2,24 @@
 
 # install minimum requirements
 
+# apt-fast repo
+echo "configuring apt-fast repo"
 [ -e "/etc/apt/sources.list.d/apt-fast.list" ] || {
       echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main\ndeb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main" > /etc/apt/sources.list.d/apt-fast.list &&
-      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B; }
+      apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A2166B8DE8BDC3367D1901C11EE2FF37CA8DA16B &&
+      echo "done"; }
+
+# llvm repo
+echo "configuring llvm toolchain repo"
+[ -e "/etc/apt/sources.list.d/llvm-toolchain-buster-13.list" ] || {
+      echo "deb http://apt.llvm.org/buster/ llvm-toolchain-buster-13 main\ndeb-src http://apt.llvm.org/buster/ llvm-toolchain-buster-13 main" > /etc/apt/sources.list.d/llvm-toolchain-buster-13.list &&
+      wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - &&
+      echo "done"; }
 
 package_list="apt-fast git curl wget tree htop python3
               python3-pip zsh zsh-syntax-highlighting
               zsh-autosuggestions ranger build-essential
-              gdb clang-tools-11"
+              gdb clang-tools-14"
 apt update || { echo "failed to update the package list"; exit 1; }
 
 echo "installing packages ..."
